2d7af71d25f8465dedf531c27ff14b974d193a7c
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-03-10 17:31:20 +0900

    양방향 연관관계 매핑

--- src/main/java/HelloJpa/JpaMain.java
+++ src/main/java/HelloJpa/JpaMain.java
@@ -1,35 +1,47 @@
-//package HelloJpa;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//import java.util.List;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//
-//        EntityManager em = emf.createEntityManager();
-//
-//        //JPA의 모든 데이터 변경은 트랜잭션에서 움직여야함 중요! 그러므로 데이터변경은 트랜잭션안에서 실행해야함
-//        EntityTransaction tx = em.getTransaction();
-//        tx.begin();
-//
-//        try {
-//            Member member = new Member();
-//
-//            member.setUsername("jpaa2a");
-//            em.persist(member);
-//
-//            tx.commit();
-//        } catch (Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//        emf.close();
-//    }
-//}
+package HelloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        //JPA의 모든 데이터 변경은 트랜잭션에서 움직여야함 중요! 그러므로 데이터변경은 트랜잭션안에서 실행해야함
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member2");
+            member.changeTeam(team); // 양쪽에 값 넣어줌 편의메소드 사용
+            em.persist(member);
+
+
+            em.flush();
+            em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId()); //1차 캐시
+            List<Member> members = findTeam.getMembers();
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,55 +1,55 @@
-package jpabook.jpashop.domain;
-
-import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "ORDERS")
-public class Order {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name ="ORDER_ID")
-    private Long id;
-
-//    @Column(name ="MEMBER_ID")
-//    private Long memberId; //누가 주문했는지
-
-    @OneToMany
-    @JoinColumn(name = "MEMBER_ID")
-    private Member member;
-
-    public Member getMember() {
-        return member;
-    }
-
-    private LocalDateTime orderDate;
-
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-}
+//package jpabook.jpashop.domain;
+//
+//import javax.persistence.*;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "ORDERS")
+//public class Order {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name ="ORDER_ID")
+//    private Long id;
+//
+////    @Column(name ="MEMBER_ID")
+////    private Long memberId; //누가 주문했는지
+//
+//    @OneToMany
+//    @JoinColumn(name = "MEMBER_ID")
+//    private Member member;
+//
+//    public Member getMember() {
+//        return member;
+//    }
+//
+//    private LocalDateTime orderDate;
+//
+//    @Enumerated(EnumType.STRING)
+//    private OrderStatus status;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public LocalDateTime getOrderDate() {
+//        return orderDate;
+//    }
+//
+//    public void setOrderDate(LocalDateTime orderDate) {
+//        this.orderDate = orderDate;
+//    }
+//
+//    public OrderStatus getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(OrderStatus status) {
+//        this.status = status;
+//    }
+//}
